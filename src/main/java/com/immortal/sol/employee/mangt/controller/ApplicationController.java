8ba4f77328d3e5bc4a2b9bfa37b524c7ea3490b7
mange <mange@Mangeeru>
2022-10-12 11:16:27 +0530

    initial commit

---
+++ src/main/java/com/immortal/sol/employee/mangt/controller/ApplicationController.java
@@ -0,0 +1,10 @@
+package com.immortal.sol.employee.mangt.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee/api")
+public class ApplicationController {
+
+}
---
+++ src/test/java/com/immortal/sol/employee/mangt/EmpManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immortal.sol.employee.mangt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
