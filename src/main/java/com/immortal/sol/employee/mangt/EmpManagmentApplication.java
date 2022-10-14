c9e5ecbb48ac67b56434d77d3ee6268e134725b9
mange <mange@Mangeeru>
2022-10-14 22:08:03 +0530

    added contact rest api and swagger doc

--- src/main/java/com/immortal/sol/employee/mangt/EmpManagmentApplication.java
+++ src/main/java/com/immortal/sol/employee/mangt/EmpManagmentApplication.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -23,7 +25,24 @@ public class EmpManagmentApplication {
 	public Docket productApi() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
 				.paths(PathSelectors.any())
-				.apis(RequestHandlerSelectors.basePackage("com.immortal.sol.employee.mangt")).build();
+				.apis(RequestHandlerSelectors
+						.basePackage("com.immortal.sol.employee.mangt"))
+				.build().apiInfo(apiInfo());
 	}
+	
+	private ApiInfo apiInfo() {
+
+		@SuppressWarnings("deprecation")
+		ApiInfo apiInfo = new ApiInfo(
+                "Employee Management",
+                "An application for employee/depatyment details contacs/address add,edit and delete ",
+                "employee mangement v1",
+                "Terms of service",
+                "immortal@gmail.com",
+                "License of API",
+                "https://swagger.io/docs/");
+
+        return apiInfo;
+    }
 
 }
--- src/main/java/com/immortal/sol/employee/mangt/repository/ContactRepository.java
+++ src/main/java/com/immortal/sol/employee/mangt/repository/ContactRepository.java
@@ -1,5 +1,7 @@
 package com.immortal.sol.employee.mangt.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.immortal.sol.employee.mangt.entity.ContactDetails;
 
 @Repository
 public interface ContactRepository extends JpaRepository<ContactDetails, Long>{
+	
+	public List<ContactDetails> findByEmpId(String empId);
 
 }
